036082e53d01720b2c7e2fb116be117512d12a06
wr <wrong2314@163.com>
2024-09-08 22:21:51 +0800

    feat: 右下角echarts

--- src/components/GlobalComponents/myCharts.vue
+++ src/components/GlobalComponents/myCharts.vue
@@ -3,36 +3,9 @@
 </template>
 
 <script setup lang="ts">
-  import { onMounted, onBeforeUnmount, ref } from "vue";
+  import { onMounted, onBeforeUnmount, ref, watch } from "vue";
   import * as echarts from "echarts";
-  // 因为是封装的组件，会多次调用，id不能重复，要在dom挂载之前定义好数据，
-  let uid = ref<string>("");
-  uid.value = `echarts-uid-${parseInt((Math.random() * 1000000).toString())}`;
-  onMounted(() => {
-    let myChart = echarts.init(document.getElementById(uid.value) as HTMLElement);
-    // 在template中可以直接取props中的值，但是在script中不行，因为script是在挂载之前执行的
-    myChart.setOption(props.myOption, {
-      notMerge: true, //不和之前的option合并
-    });
 
-    // 监听页面的大小
-    window.addEventListener("resize", () => {
-      setTimeout(() => {
-        myChart?.resize({
-          animation: {
-            duration: 300,
-          },
-        });
-      }, 300);
-    });
-  });
-
-  // 页面离开时销毁echarts实例和监听事件和定时器
-  onBeforeUnmount(() => {
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    window.removeEventListener("resize", () => {});
-    echarts.dispose(document.getElementById(uid.value) as HTMLElement);
-  });
   const props = defineProps({
     myStyle: {
       type: Object,
@@ -46,5 +19,46 @@
       default: () => ({}),
       required: true,
     },
+  });
+
+  // 因为是封装的组件，会多次调用，id不能重复，要在dom挂载之前定义好数据，
+  let uid = ref<string>("");
+  uid.value = `echarts-uid-${parseInt((Math.random() * 1000000).toString())}`;
+
+  const myChart = ref<any>(null);
+
+  watch(
+    () => props.myOption,
+    newData => {
+      myChart.value.setOption(newData, {
+        notMerge: true, //不和之前的option合并
+      });
+    }
+  );
+
+  onMounted(() => {
+    myChart.value = echarts.init(document.getElementById(uid.value) as HTMLElement);
+    // 在template中可以直接取props中的值，但是在script中不行，因为script是在挂载之前执行的
+    myChart.value.setOption(props.myOption, {
+      notMerge: true, //不和之前的option合并
+    });
+
+    // 监听页面的大小
+    window.addEventListener("resize", () => {
+      setTimeout(() => {
+        myChart.value?.resize({
+          animation: {
+            duration: 300,
+          },
+        });
+      }, 300);
+    });
+  });
+
+  // 页面离开时销毁echarts实例和监听事件和定时器
+  onBeforeUnmount(() => {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    window.removeEventListener("resize", () => {});
+    echarts.dispose(document.getElementById(uid.value) as HTMLElement);
   });
 </script>
--- src/views/home-page/components/comp-chart/comp-right/home-right-bottom.vue
+++ src/views/home-page/components/comp-chart/comp-right/home-right-bottom.vue
@@ -6,6 +6,7 @@
   import { computed, ref, watch } from "vue";
   import { useDataStore } from "@/store/dataStore";
   import axios from "axios";
+
   export interface ITimeLineData {
     name: string; //业务条线名称
     value: string; //值
@@ -15,7 +16,7 @@
   const startDate = computed(() => store.startDate);
   const endDate = computed(() => store.endDate);
 
-  const option = ref({});
+  let option = ref({});
   const seriesData = ref([]);
   const xAxisData = computed(() => seriesData.value?.[0]?.data?.map(item => item?.name));
   const colorStopsList = [
@@ -31,7 +32,7 @@
 
   const fetchTimeLine = async (indicatorId?: string, areaId?: string) => {
     try {
-      const { data } = await axios.get("/api/v1/spzx/time_trend", {
+      let { data } = await axios.get("/api/v1/spzx/time_trend", {
         params: {
           kssj: startDate.value,
           jssj: endDate.value,
@@ -39,43 +40,45 @@
           indicatorId,
         },
       });
-      // let temp = [
-      //   {
-      //     name: "本期",
-      //     data: [
-      //       { name: "1月", value: "300" },
-      //       { name: "2月", value: "400" },
-      //       { name: "3月", value: "350" },
-      //       { name: "4月", value: "250" },
-      //       { name: "5月", value: "320" },
-      //       { name: "6月", value: "450" },
-      //       { name: "7月", value: "500" },
-      //       { name: "8月", value: "450" },
-      //       { name: "9月", value: "280" },
-      //       { name: "10月", value: "200" },
-      //       { name: "11月", value: "230" },
-      //       { name: "12月", value: "280" },
-      //     ],
-      //   },
-      //   {
-      //     name: "同期",
-      //     data: [
-      //       { name: "1月", value: "180" },
-      //       { name: "2月", value: "200" },
-      //       { name: "3月", value: "350" },
-      //       { name: "4月", value: "500" },
-      //       { name: "5月", value: "400" },
-      //       { name: "6月", value: "350" },
-      //       { name: "7月", value: "220" },
-      //       { name: "8月", value: "280" },
-      //       { name: "9月", value: "420" },
-      //       { name: "10月", value: "380" },
-      //       { name: "11月", value: "350" },
-      //       { name: "12月", value: "400" },
-      //     ],
-      //   },
-      // ];
-      // const data = { data: temp };
+
+      data = {
+        data: [
+          {
+            name: "本期",
+            data: [
+              { name: "1月", value: "300" },
+              { name: "2月", value: "400" },
+              { name: "3月", value: "350" },
+              { name: "4月", value: "250" },
+              { name: "5月", value: "320" },
+              { name: "6月", value: "450" },
+              { name: "7月", value: "500" },
+              { name: "8月", value: "450" },
+              { name: "9月", value: "280" },
+              { name: "10月", value: "200" },
+              { name: "11月", value: "230" },
+              { name: "12月", value: "280" },
+            ],
+          },
+          {
+            name: "同期",
+            data: [
+              { name: "1月", value: "180" },
+              { name: "2月", value: "200" },
+              { name: "3月", value: "350" },
+              { name: "4月", value: "500" },
+              { name: "5月", value: "400" },
+              { name: "6月", value: "350" },
+              { name: "7月", value: "220" },
+              { name: "8月", value: "280" },
+              { name: "9月", value: "420" },
+              { name: "10月", value: "380" },
+              { name: "11月", value: "350" },
+              { name: "12月", value: "400" },
+            ],
+          },
+        ],
+      };
 
       seriesData.value = (data?.data || []).map((item: any[]) => ({
         ...item,
